e7e2a4d7a40ceeb75ea4c6d52873bee6f63b4a90
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-07-05 04:22:03 +1200

    Project setup

---
+++ src/main/java/shellymutu_grigg/Selenium_WebDriver_with_Java_Section26_Apache/App.java
@@ -0,0 +1,13 @@
+package shellymutu_grigg.Selenium_WebDriver_with_Java_Section26_Apache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/shellymutu_grigg/Selenium_WebDriver_with_Java_Section26_Apache/AppTest.java
@@ -0,0 +1,20 @@
+package shellymutu_grigg.Selenium_WebDriver_with_Java_Section26_Apache;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
